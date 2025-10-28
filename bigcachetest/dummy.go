cbced5dfbac285ad5b1f3a87cb060a82be7e2f59
lmvysakh <lmvysakh@github.com>
2025-10-28 13:32:56 +0530

    Updating workflow

---
+++ bigcachetest/dummy.go
@@ -0,0 +1,37 @@
+// bigcachetest/dummy.go
+package main
+
+import (
+    _ "github.com/aws/aws-sdk-go-v2/aws"
+    _ "cloud.google.com/go"
+    _ "github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+    _ "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute"
+    _ "github.com/lib/pq"
+    _ "github.com/go-sql-driver/mysql"
+    _ "github.com/golang/protobuf/proto"
+    _ "google.golang.org/grpc"
+    _ "github.com/Shopify/sarama"
+    _ "github.com/elastic/go-elasticsearch/v8"
+    _ "github.com/hashicorp/terraform/helper/schema"
+    _ "github.com/uber-go/zap"
+    _ "github.com/prometheus/client_golang/prometheus"
+    _ "github.com/gin-gonic/gin"
+    _ "github.com/go-redis/redis/v8"
+    _ "github.com/gorilla/websocket"
+    _ "github.com/sirupsen/logrus"
+    _ "github.com/spf13/cobra"
+    _ "github.com/spf13/viper"
+    _ "go.mongodb.org/mongo-driver/mongo"
+    _ "github.com/google/uuid"
+    _ "github.com/minio/minio-go/v7"
+    _ "github.com/docker/docker/client"
+    _ "github.com/kubernetes/client-go/kubernetes"
+    _ "github.com/coreos/etcd/clientv3"
+    _ "github.com/apache/thrift/lib/go/thrift"
+    _ "github.com/apache/arrow/go/arrow"
+    _ "github.com/nats-io/nats.go"
+    _ "github.com/segmentio/kafka-go"
+    _ "github.com/googleapis/gax-go/v2"
+    _ "github.com/gocql/gocql"
+)
+func main() {}
